2829d332a9a07904d1551ded1bd998725d6880a7
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-18 14:50:36 +0200

    feat: test Js - Funzionante

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const userName = prompt("inserisci nome utente");
+const userName = prompt("Inserisci nome utente");
 const userSurname = prompt("Inserisci il cognome");
 const userAge = prompt("Inserisci la tua età");
 const userColor = prompt("Inserisci il tuo colore");
@@ -6,5 +6,9 @@ const userColor = prompt("Inserisci il tuo colore");
 const welcomeMessage = `
 <h3>Benvenuto ${userName + userSurname + userAge + userColor}</h3>
 `;
+
+alert(welcomeMessage);
+
+console.log(userName + userSurname + userAge + userColor);
 
 document.getElementById("user-feedback").innerHTML = welcomeMessage;
