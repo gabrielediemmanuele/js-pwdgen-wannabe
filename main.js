9483911857a2a4641faafdb9f7ac34d2fddbf6cd
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-18 14:57:04 +0200

    feat: add some bonuses

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ alert(welcomeMessage);
 console.log(userName + userSurname + userAge + userColor);
 
 document.getElementById("user-feedback").innerHTML = welcomeMessage;
+
+//ESPERIMENTO
+
+let userPresentAge = parseInt(prompt("Inserisci la tua età"));
+let userBirthYear = parseInt(prompt("Inserisci anno di nascita"));
+
+const currentYear = userPresentAge + userBirthYear;
+alert("Il tuo anno corrente è:" + currentYear);
+console.log("Il tuo anno corrente è:" + currentYear);
